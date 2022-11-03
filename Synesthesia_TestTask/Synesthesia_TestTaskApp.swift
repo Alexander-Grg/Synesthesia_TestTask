b5303bda95675902539dfd46652c2e8bcbc5f05e
Alexander <a.a.grigoryev@icloud.com>
2022-11-03 10:32:50 +0300

    Initial Commit

---
+++ Synesthesia_TestTask/Synesthesia_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Synesthesia_TestTaskApp.swift
+//  Synesthesia_TestTask
+//
+//  Created by Alexander Grigoryev on 3.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Synesthesia_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
